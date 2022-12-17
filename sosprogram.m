545da9bff1809e4e766cacc2a9b787e74dfdcd66
Declan <91631433+djagt@users.noreply.github.com>
2022-12-17 14:07:16 -0700

    Revert "Add Mosek to SDP solver check in sosprogram."
    This reverts commit ef2fbf1978ad222a49f5403ffe793efad96376f0.

--- sosprogram.m
+++ sosprogram.m
@@ -47,7 +47,7 @@ function sos = sosprogram(vartable,decvartable)
 % 02/21/02 - SP -- Symbolic polynomial
 % 10/10/02 - SP -- Path checking 
 
-if ~exist('sedumi') & ~exist('sqlp') & ~exist('csdp') & ~exist('sdpnal') & ~exist('sdpnalplus')&  ~exist('sdpam') &  ~exist('cdcs') & ~exist('mosekopt')
+if ~exist('sedumi') & ~exist('sqlp') & ~exist('csdp') & ~exist('sdpnal') & ~exist('sdpnalplus')&  ~exist('sdpam') &  ~exist('cdcs')
     error('No SDP solvers found.') ;
 end;
 
